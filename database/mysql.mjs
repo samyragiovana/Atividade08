f8853215389906a45dd954769438cf556d622a46
Samyra Giovana <96441155+samyragiovana@users.noreply.github.com>
2024-08-14 14:51:41 -0300

    Update mysql.mjs

--- database/mysql.mjs
+++ database/mysql.mjs
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("mysql://root:root@localhost:3306/dewii2024");
-//const sequelize = new Sequelize("mysql://root@localhost:3306/dewii2024node");
+//const sequelize = new Sequelize("mysql://root:root@localhost:3306/dewii2024");
+const sequelize = new Sequelize("postgresql://user_db:90PvES0GexVbg6DltvbZ0QAiJAEurkoG@dpg-cquesmtsvqrc73bnhvbg-a/veiculos_o28e");
 
 sequelize.sync();
 
